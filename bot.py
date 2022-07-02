64062144b17f35dbf5bbca05203985396d624d5f
shlomigd <shlomigd@gmail.com>
2022-07-02 17:05:32 +0300

    YoutubeBot remove secret

--- bot.py
+++ bot.py
@@ -1,99 +1,49 @@
 from telegram.ext import Updater, MessageHandler, Filters
-import boto3
-import base64
-from botocore.exceptions import ClientError
-import bot
 from utils import search_download_youtube_video
 from loguru import logger
+import bot
 
 
 class Bot:
 
     def __init__(self, token):
-        # create frontend object to the bot programmer
-        self.updater = Updater(token, use_context=True)
+            # create frontend object to the bot programmer
+            self.updater = Updater(token, use_context=True)
 
-        # add _message_handler as main internal msg handler
-        self.updater.dispatcher.add_handler(MessageHandler(Filters.text, self._message_handler))
+            # add _message_handler as main internal msg handler
+            self.updater.dispatcher.add_handler(MessageHandler(Filters.text, self._message_handler))
 
     def start(self):
-        """Start polling msgs from users, this function never returns"""
-        self.updater.start_polling()
-        logger.info(f'{self.__class__.__name__} is up and listening to new messages....')
-        self.updater.idle()
+            """Start polling msgs from users, this function never returns"""
+            self.updater.start_polling()
+            logger.info(f'{self.__class__.__name__} is up and listening to new messages....')
+            self.updater.idle()
 
     def _message_handler(self, update, context):
-        """Main messages handler"""
-        self.send_text(update, f'Your original message: {update.message.text}')
+            """Main messages handler"""
+            self.send_text(update, f'Your original message: {update.message.text}')
 
     def send_video(self, update, context, file_path):
-        """Sends video to a chat"""
-        context.bot.send_video(chat_id=update.message.chat_id, video=open(file_path, 'rb'), supports_streaming=True)
+            """Sends video to a chat"""
+            context.bot.send_video(chat_id=update.message.chat_id, video=open(file_path, 'rb'), supports_streaming=True)
 
     def send_text(self, update,  text, quote=False):
-        """Sends text to a chat"""
-        # retry https://github.com/python-telegram-bot/python-telegram-bot/issues/1124
-        update.message.reply_text(text, quote=quote)
+            """Sends text to a chat"""
+            # retry https://github.com/python-telegram-bot/python-telegram-bot/issues/1124
+            update.message.reply_text(text, quote=quote)
 
-    def get_secret(self):
-
-        secret_name = "shlomiTelegram"
-        region_name = "eu-central-1"
-
-        # Create a Secrets Manager client
-        session = boto3.session.Session()
-        client = session.client(
-            service_name='secretsmanager',
-            region_name=region_name
-        )
-
-        # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
-        # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        # We rethrow the exception by default.
-
-        try:
-            get_secret_value_response = client.get_secret_value(
-                SecretId=secret_name
-            )
-        except ClientError as e:
-            if e.response['Error']['Code'] == 'DecryptionFailureException':
-                # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-                # Deal with the exception here, and/or rethrow at your discretion.
-                raise e
-            elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-                # An error occurred on the server side.
-                # Deal with the exception here, and/or rethrow at your discretion.
-                raise e
-            elif e.response['Error']['Code'] == 'InvalidParameterException':
-                # You provided an invalid value for a parameter.
-                # Deal with the exception here, and/or rethrow at your discretion.
-                raise e
-            elif e.response['Error']['Code'] == 'InvalidRequestException':
-                # You provided a parameter value that is not valid for the current state of the resource.
-                # Deal with the exception here, and/or rethrow at your discretion.
-                raise e
-            elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-                # We can't find the resource that you asked for.
-                # Deal with the exception here, and/or rethrow at your discretion.
-                raise e
-        else:
-            # Decrypts secret using the associated KMS key.
-            # Depending on whether the secret is a string or binary, one of these fields will be populated.
-            if 'SecretString' in get_secret_value_response:
-                secret = get_secret_value_response['SecretString']
-            else:
-                decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
 
 class QuoteBot(Bot):
     def _message_handler(self, update, context):
         to_quote = True
 
         if update.message.text == 'Don\'t quote me please':
-           to_quote = False
-           self.send_text(update, f'Your original message: {update.message.text}', quote=to_quote)
+            to_quote = False
+            self.send_text(update, f'Your original message: {update.message.text}', quote=to_quote)
 
 
 class YoutubeBot(Bot):
+
 
     def _message_handler(self, update, context):
         path_list = []
@@ -104,11 +54,9 @@ class YoutubeBot(Bot):
 
 
 if __name__ == '__main__':
-    """
-    with open('.telegramToken') as f:
-         _token = f.read()
-         """
-    _token = bot.Bot.get_secret()
-    my_bot = bot.YoutubeBot(_token)
-    my_bot.start()
+        with open('.telegramToken') as f:
+            _token = f.read()
+
+        my_bot = bot.YoutubeBot(_token)
+        my_bot.start()
 
