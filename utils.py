a7d55e5469d1c644a3aa0135108f3e544872ee22
itayben <itaybenb222@gmail.com>
2022-08-16 22:30:34 +0300

    polybot lest ver 16/8/22

--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@ import time
 from yt_dlp import YoutubeDL
 from loguru import logger
 import boto3
+import botocore
+import os
+import string
 
 
 def search_download_youtube_video(video_name, num_results=1):
@@ -11,8 +14,43 @@ def search_download_youtube_video(video_name, num_results=1):
     :param num_results: integer representing how many videos to download
     :return: list of paths to your downloaded video files
     """
+    #with YoutubeDL() as ydl:
+    #    videos = ydl.extract_info(f"ytsearch{num_results}:{video_name}", download=True)['entries']
+    #return [ydl.prepare_filename(video) for video in videos]
+
     with YoutubeDL() as ydl:
-        videos = ydl.extract_info(f"ytsearch{num_results}:{video_name}", download=True)['entries']
+        videos = ydl.extract_info(f"ytsearch{num_results}:{video_name}", download=False)['entries']
+        for video in videos:
+            key = "dir-1/"
+            video_title = str(video['title'])
+            for char in string.punctuation:
+                video_title = video_title.replace(char, '')
+            file_name = video_title + " [" + video['id'] + "].mp4"
+            #file_name = video['title'] + " [" + video['id'] + "].mp4"
+            #file_name = file_name.replace('|', "")
+            key_value = key + file_name
+            print(key_value)
+            s3_res = boto3.resource('s3')
+            s3 = boto3.client('s3')
+            try:
+                s3_res.Object('itay-bucket1', key_value).load()
+            except botocore.exceptions.ClientError as e:
+                if e.response['Error']['Code'] == "404":
+                    print("The object does not exist.")
+                    video_url = video['webpage_url']
+                    ydl.extract_info(video_url, download=True)
+                    files = os.listdir('.')
+                    for f in files:
+                        if '.mp4' in f:
+                            newname = file_name
+                            os.rename(f, newname)
+                    s3.upload_file(Bucket='itay-bucket1', Key=key_value, Filename=file_name)
+                    os.remove(file_name)
+                else:
+                    print("Something else has gone wrong.")
+                    raise
+            else:
+                print("The object does exist.")
 
     return [ydl.prepare_filename(video) for video in videos]
 
@@ -25,30 +63,21 @@ def calc_backlog_per_instance(sqs_queue_client, asg_client, asg_group_name):
         if msgs_in_queue == 0:
             backlog_per_instance = 0
         elif asg_size == 0:
-            backlog_per_instance = 10
+            backlog_per_instance = 99
         else:
             backlog_per_instance = msgs_in_queue / asg_size
 
         logger.info(f'backlog per instance: {backlog_per_instance}')
-        cloudwatch = boto3.client('cloudwatch')
 
+        cloudwatch = boto3.client('cloudwatch')
         cloudwatch.put_metric_data(
+            Namespace='itay?',
             MetricData=[
                 {
-                    'MetricName': 'PAGES_VISITED',
-                    'Dimensions': [
-                        {
-                            'Name': 'UNIQUE_PAGES',
-                            'Value': 'URLS'
-                        },
-                    ],
-                    'Unit': 'None',
-                    'Value': 1.0
-                },
-            ],
-            Namespace='itay?'
+                    'MetricName': 'backlog_per_instance',
+                    'Value':  backlog_per_instance,
+                    'Unit': 'Count'
+                }
+            ]
         )
-
-        # TODO send the backlog_per_instance metric to cloudwatch
-
         time.sleep(60)
