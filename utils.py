885aaa947633ebdb476c7528d8a46c2619033e36
ShayKllifAlon <kllif@hotmail.com>
2022-11-03 11:54:56 +0200

    addind unitest with calc_backlog_per_instance_periodically logical function to utill

--- utils.py
+++ utils.py
@@ -19,11 +19,28 @@ def search_download_youtube_video(video_name, num_results=1):
 
 
 def calc_backlog_per_instance(sqs_queue_client, asg_client, asg_group_name):
+    msgs_in_queue = int(sqs_queue_client.attributes.get('ApproximateNumberOfMessages'))
+    asg_size = asg_client.describe_auto_scaling_groups(AutoScalingGroupNames=[asg_group_name])['AutoScalingGroups'][0][
+        'DesiredCapacity']
+
+    if msgs_in_queue == 0:
+        backlog_per_instance = 0
+    elif asg_size == 0:
+        backlog_per_instance = 99
+    else:
+        backlog_per_instance = msgs_in_queue / asg_size
+
+    return backlog_per_instance
+
+
+def calc_backlog_per_instance_periodically(sqs_queue_client, asg_client, asg_group_name):
     with open('config.json') as f:
         config = json.load(f)
     while True:
         msgs_in_queue = int(sqs_queue_client.attributes.get('ApproximateNumberOfMessages'))
-        asg_size = asg_client.describe_auto_scaling_groups(AutoScalingGroupNames=[asg_group_name])['AutoScalingGroups'][0]['DesiredCapacity']
+        asg_size = \
+        asg_client.describe_auto_scaling_groups(AutoScalingGroupNames=[asg_group_name])['AutoScalingGroups'][0][
+            'DesiredCapacity']
 
         if msgs_in_queue == 0:
             backlog_per_instance = 0
@@ -33,8 +50,6 @@ def calc_backlog_per_instance(sqs_queue_client, asg_client, asg_group_name):
             backlog_per_instance = msgs_in_queue / asg_size
 
         logger.info(f'backlog per instance: {backlog_per_instance}')
-
-        # TODO send the backlog_per_instance metric to cloudwatch
 
         boto3.client('cloudwatch', region_name=config.get('aws_region')).put_metric_data(
             MetricData=[
