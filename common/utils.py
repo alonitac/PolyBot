ca962bc2bbddaf002c140e21ab1570a7bf053a1f
ShayKllifAlon <kllif@hotmail.com>
2022-11-08 14:58:09 +0200

    commit on /common dir 6

--- common/utils.py
+++ common/utils.py
@@ -6,6 +6,7 @@ import json
 from yt_dlp import YoutubeDL
 
 
+
 def search_download_youtube_video(video_name, num_results=1):
     """
     This function downloads the first num_results search results from Youtube
